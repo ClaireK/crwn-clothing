4d36ee04b8225f82275143d442f22399e9375191
ClaireK <c_l_king@hotmail.com>
2019-10-18 20:55:14 +0200

    implemented firebase utils, added authenticated user to db

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -12,6 +12,32 @@ const config = {
   appId: "1:1073736075959:web:77ab781167c26bc2649b32"
 };
 
+export const createUserProfileDocument = async (userAuth, additionalDate) => {
+  if (!userAuth) return;
+
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+  const snapShot = await userRef.get();
+
+  if (!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalDate
+      });
+    } catch (error) {
+      console.log("error creating user", error.message);
+    }
+  }
+
+  return userRef;
+};
+
 firebase.initializeApp(config);
 
 export const auth = firebase.auth();
